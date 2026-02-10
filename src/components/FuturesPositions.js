2a367898f002b41ff8f8bedd46eb7bfa170d2d5c
TheVasuA <thevasu.a@gmail.com>
2026-02-10 18:41:59 +0530

    Update FuturesPositions.js: formatting and latest changes

--- src/components/FuturesPositions.js
+++ src/components/FuturesPositions.js
@@ -383,7 +383,7 @@ export default function FuturesPositions({ positions, onRefresh, pendingOrders =
                 <tr key={`open-entry-${order.orderId || idx}`} className="border-b border-blue-700 bg-blue-900/30">
                   <td className="py-3 px-4 text-blue-200">
                     {order.symbol}
-                    <span className={`ml-2 px-2 py-0.5 rounded text-xs font-semibold ${order.side === 'BUY' ?  'bg-red-500/20 text-red-400': 'bg-green-500/20 text-green-400'}`}>{order.side}</span>
+                    <span className={`ml-2 px-2 py-0.5 rounded text-md font-semibold ${order.side === 'buy' ?   'bg-green-500/20 text-green-400':'bg-red-500/20 text-red-400'}`}>{order.side}</span>
                   </td>
                   <td className="py-3 px-4 text-blue-200">{(order.amount * order.price).toLocaleString('en-US', { maximumFractionDigits: 8 })}</td>
                   <td className="py-3 px-4 text-blue-200">{order.price}</td>
